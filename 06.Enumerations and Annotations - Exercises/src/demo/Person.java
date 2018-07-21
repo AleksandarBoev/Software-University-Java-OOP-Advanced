716d3ff42e7b297473b083c4b3cf96c9aa1fcf7c
sa6o sa6o <aleksandarboev95@gmail.com>
2018-07-21 10:28:10 +0300

    Added new project

---
+++ 06.Enumerations and Annotations - Exercises/src/demo/Person.java
@@ -0,0 +1,9 @@
+package demo;
+
+public class Person {
+    private String[] interests;
+
+    public void setInterests(String[] interests) {
+        this.interests = interests;
+    }
+}
---
+++ 06.Enumerations and Annotations - Exercises/src/p01_harvestingFields/Main.java
@@ -0,0 +1,7 @@
+package p01_harvestingFields;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
